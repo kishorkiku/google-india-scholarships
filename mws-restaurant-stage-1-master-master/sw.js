f11c22de9fd12e128f9eb62550a7efbd4f008f0a
kishor kumar <31623726+kishorkiku@users.noreply.github.com>
2018-11-01 16:13:35 +0530

    Update sw.js

--- mws-restaurant-stage-1-master-master/sw.js
+++ mws-restaurant-stage-1-master-master/sw.js
@@ -1,50 +1,55 @@
+console.log('Service Worker: Registered');
 
-var cache_name = 'resturant-cache';
 
-// install cache
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
-  event.waitUntil(
-    caches.open(cache_name).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/restaurant.html',
-        '/css/styles.css',
-        '/js/dbhelper.js',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        '/data/restaurants.json',
-        '/img/1.jpg',
-        '/img/2.jpg',
-        '/img/3.jpg',
-        '/img/4.jpg',
-        '/img/5.jpg',
-        '/img/6.jpg',
-        '/img/7.jpg',
-        '/img/8.jpg',
-        '/img/9.jpg',
-        '/img/10.jpg'
-      ]);
-    })
-  );
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
 
-// activate cache
-
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            if (response) {
+                console.log('Found', e.request, 'in cache, FETCHING!');
+                return fetch(e.request);
+            } else {
+                console.log('could not find', e.request, 'in cache, FETCHING!');
+                return fetch(e.request)
+                    .then(function (response) {
+                        const clonedResponse = response.clone();
+                        caches.open('v1').then(function (cache) {
+                            cache.put(e.request, clonedResponse);
+                        })
+                        return response;
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    })
+            }
+        })
+    );
 });
 
-// fetch
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
